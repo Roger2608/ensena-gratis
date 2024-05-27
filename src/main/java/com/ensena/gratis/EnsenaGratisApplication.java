696f4d32a96a2b6627f358add3db7d6be9c7718e
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2024-05-26 23:26:05 -0500

    add commit initial

---
+++ src/main/java/com/ensena/gratis/EnsenaGratisApplication.java
@@ -0,0 +1,13 @@
+package com.ensena.gratis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsenaGratisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EnsenaGratisApplication.class, args);
+	}
+	
+}
